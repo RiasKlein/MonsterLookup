637b501875ffde8731fb444eda25b9f4fe4becbf
Rias <tseshunman@gmail.com>
2016-04-27 03:52:20 -0400

    Launcher Program available
    - Monsters will be added over time

---
+++ launcher.py
@@ -0,0 +1,90 @@
+import os
+import sys
+import webbrowser
+
+# Configurable Global Settings
+file_monster_list = 'monster_list.txt'	# file with the monster HTML file names
+system_version = '0.1'
+
+# readMonsterList
+#	Function reads the file monster_list.txt for all currently available monsters
+#	Each entry is added into the list called monster_list
+def readMonsterList ():
+	# Open 'monster_list.txt' in reading mode
+	rfile = open (file_monster_list, 'r')
+	
+	# We'll keep going until we reach the end of the file
+	while True:
+		line = rfile.readline()		# Get a line from 'monster_list.txt'
+		if not line: return			# End the function if we're at the end
+
+		if ('.html' in line):
+			line = line.rstrip ('.html\n')			# Remove the ending from each line
+			monster_list.append (line.lower())		# Append as entry into the list of monsters
+
+# handleBuiltIn
+#	Function handles the built-in functions	
+#	Returns whether the provided input is a built-in function	
+def handleBuiltIn ( user_input ):
+	is_builtin = False
+
+	# If the input was 'help', then provide the available monster list
+	if (user_input.lower() == 'help'):
+		is_builtin = True
+		print ("============================\n\tMonster List\n============================")
+		print (monster_list)
+		print ("")
+		print ("System Version: " + system_version)
+	# If the input was 'exit', then exit the system
+	elif (user_input.lower() == 'exit'):
+		print ("Exiting...")
+		sys.exit()
+		
+	return is_builtin
+
+# searchAndOpen
+#	Function opens the monster file for the monster if it exists
+#	Feedback is given regardless
+def searchAndOpen ( user_input ):
+	# See if the monster name is available in the monster list
+	if user_input.lower() in monster_list:
+		# If the monster name is found in the list of monsters: 
+		#	Open the proper file in the browser
+		name = user_input + '.html'
+		webbrowser.open_new_tab (name)	
+	else:
+		# If the monster name was not found, then give output saying so
+		print ("Monster Not Found")
+
+# printIntroduction
+#	Function prints out basic information displayed at the start		
+def printIntroduction ():
+	print ("========================= D&D 5e Monster Lookup =========================")
+	print ("Type in a monster name to look for or type in 'help' for more information")
+		
+def main():
+	# Change the working directory to the Monsters folder
+	os.chdir ('./Monsters')
+
+	# First, let's get the names of all available monsters
+	readMonsterList()
+	
+	# Introduction Information!
+	printIntroduction ()
+	
+	# Get user searches until they are done
+	while True:
+		# Ask the user for what they are searching for
+		sys.stdout.write('>')
+		
+		# Get the user input
+		user_input = raw_input ()
+		
+		# Handle the user input
+		# 	Built-in functions are executed
+		#	Anything else are treated as searches
+		if (not handleBuiltIn (user_input)): 
+			searchAndOpen (user_input)
+		
+monster_list = []	# List of monster names that we have available
+main()
